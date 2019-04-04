dc4f34849daf037a42895efbc26db9b0d16aad6c
James Clark <jamesclark@Jamess-MacBook-Pro.local>
2019-04-04 14:50:06 -0400

    can now avoid printing with graphviz

--- hamt/README.md
+++ hamt/README.md
@@ -1,3 +1,17 @@
-A basic implementation of the HAMT data structure.
+A basic implementation of the HAMT data structure. To allow pretty printing of the data structure graphviz must be installed along with python.
 
-Compile code using by typing make on the command line then ./hamt .
+Compile and run with:
+
+```
+> make
+
+> ./go
+```
+
+To compile and run with out graphviz and python:
+
+```
+> make
+
+> ./go -b
+```
--- hamt/go.ml
+++ hamt/go.ml
@@ -1,9 +1,20 @@
 open Hamt
 open Helper
 
+let options =
+  match (Array.length Sys.argv) > 1 with
+  | false -> false
+  | true ->
+    begin
+      match Sys.argv.(1) with
+      | "-b" -> true
+      | _ -> false
+    end
+
 let go () =
   let testHamt2 = initq 40 in
-  Printf.printf "shiftvalue: %d\n" shiftvalue;
-  export testHamt2
+  match options with
+  | false -> export testHamt2
+  | true -> 1
 
 let s = go ()
